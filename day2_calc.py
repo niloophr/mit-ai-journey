d43ef2fd6db077b9ece1157ce2cc63f050790a03
niloophr <niloophr.keshavarz@gmail.com>
2025-11-18 09:21:43 -0800

    Create day2_calc.py

---
+++ day2_calc.py
@@ -0,0 +1,17 @@
+name1 = input("What is your name? ")
+family1 = input("What is your family name? ")
+print("Hello", name1, family1)
+
+age = int(input("How old are you? "))
+print("Oh!! Two years later you are", age + 2)
+
+print("Give me two float numbers")
+Num1 = float(input("Enter First Number: "))
+Num2 = float(input("Enter Second Number: "))
+
+print("Num1 + Num2 =", Num1 + Num2)
+print("Num1 - Num2 =", Num1 - Num2)
+print("Num1 / Num2 =", Num1 / Num2)
+print("Num1 * Num2 =", Num1 * Num2)
+
+print("Name:", name1, "Family:", family1, "Age:", age, sep=" - ")
